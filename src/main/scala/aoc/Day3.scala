b6a2ed8527cf4c0654768c86d4f20ecf56ed2f10
n3ziniuka5 <laurynas.tretjakovas@gmail.com>
2022-12-03 09:43:43 +0200

    Day 3

---
+++ src/main/scala/aoc/Day3.scala
@@ -0,0 +1,40 @@
+package aoc
+
+import aoc.Common.{enqueueAndKeepMaxSize, timed}
+
+import scala.annotation.tailrec
+import scala.collection.mutable
+import scala.io.Source
+
+object Day3:
+  def main(args: Array[String]): Unit =
+    val lines = Source.fromResource("day3.txt").getLines().toList
+    timed("Part 1", part1(lines))
+    timed("Part 2", part2(lines))
+
+  def charPriority(c: Char): Int =
+    if (c.isUpper) c.toInt - 38
+    else c.toInt - 96
+
+  def part1(lines: List[String]): Int =
+    lines
+      .map { line =>
+        val (r1, r2)  = line.splitAt(line.size / 2)
+        val commonSet = r1.toSet & r2.toSet
+        commonSet.head
+      }
+      .map(charPriority)
+      .sum
+
+  def part2(lines: List[String]): Int =
+    lines
+      .sliding(3, 3)
+      .map { group =>
+        group
+          .foldLeft(('a' to 'z').toSet ++ ('A' to 'Z').toSet)((commonItems, currentBag) =>
+            commonItems & currentBag.toSet
+          )
+          .head
+      }
+      .map(charPriority)
+      .sum
---
+++ src/test/scala/aoc/Day3Test.scala
@@ -0,0 +1,19 @@
+package aoc
+
+import zio.test.{ZIOSpecDefault, assertTrue}
+
+object Day3Test extends ZIOSpecDefault {
+  val input = List(
+    "vJrwpWtwJgWrhcsFMMfFFhFp",
+    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+    "PmmdzqPrVvPwwTWBwg",
+    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+    "ttgJtRGJQctTZtZT",
+    "CrZsJsPPZsGzwwsLwLmpwMDw"
+  )
+
+  override def spec = suite("Day 1")(
+    test("Part 1")(assertTrue(Day3.part1(input) == 157)),
+    test("Part 2")(assertTrue(Day3.part2(input) == 70))
+  )
+}
