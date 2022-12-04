e8aa26e0dae04394aac0984727bd457e5676824d
n3ziniuka5 <laurynas.tretjakovas@gmail.com>
2022-12-04 08:57:12 +0200

    Day 4

--- src/main/scala/aoc/Day4.scala
+++ src/main/scala/aoc/Day4.scala
@@ -6,14 +6,13 @@ import scala.annotation.tailrec
 import scala.collection.mutable
 import scala.io.Source
 
-object Day4 {
-  def main(args: Array[String]): Unit = {
+object Day4:
+  def main(args: Array[String]): Unit =
     val lines = Source.fromResource("day4.txt").getLines().toList
     timed("Part 1", part1(lines))
     timed("Part 2", part2(lines))
-  }
 
-  def parseRanges(line: String): (Range, Range) = {
+  def parseRanges(line: String): (Range, Range) =
     val Array(r1, r2) = line
       .split(',')
       .map { rangeString =>
@@ -23,21 +22,15 @@ object Day4 {
       .sortBy(r => (r.start, -r.end))
 
     (r1, r2)
-  }
 
-  def fullyOverlap(r1: Range, r2: Range): Boolean = {
+  def fullyOverlap(r1: Range, r2: Range): Boolean =
     r1.end >= r2.end
-  }
 
-  def overlap(r1: Range, r2: Range): Boolean = {
+  def overlap(r1: Range, r2: Range): Boolean =
     r1.end >= r2.start
-  }
 
-  def part1(lines: List[String]): Int = {
+  def part1(lines: List[String]): Int =
     lines.map(parseRanges).count(fullyOverlap)
-  }
 
-  def part2(lines: List[String]): Int = {
+  def part2(lines: List[String]): Int =
     lines.map(parseRanges).count(overlap)
-  }
-}
