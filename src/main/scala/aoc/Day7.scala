300aae01a0a87f14d47a5ee6dd7df690401698b2
n3ziniuka5 <laurynas.tretjakovas@gmail.com>
2022-12-07 08:57:36 +0200

    Day 7

---
+++ src/main/scala/aoc/Day7.scala
@@ -0,0 +1,51 @@
+package aoc
+
+import aoc.Common.timed
+
+import scala.annotation.tailrec
+import scala.io.Source
+
+object Day7:
+  def main(args: Array[String]): Unit =
+    val lines = Source.fromResource("day7.txt").getLines().toList
+    timed("Part 1", part1(lines))
+    timed("Part 2", part2(lines))
+
+  val cdR = "\\$ cd (.+)".r
+
+  @tailrec
+  def unfoldPath[A](path: List[String], initial: A, unfold: (A, List[String]) => A): A =
+    if (path.isEmpty) initial
+    else unfoldPath(path.tail, unfold(initial, path), unfold)
+
+  @tailrec
+  def sizeOfDir(
+    input: List[String],
+    currentPath: List[String],
+    result: Map[List[String], Int]
+  ): Map[List[String], Int] =
+    input match
+      case head :: tail =>
+        if (head == "$ cd ..") sizeOfDir(tail, currentPath.tail, result)
+        else if (head.startsWith("$ ls")) sizeOfDir(tail, currentPath, result)
+        else if (head.head.isDigit)
+          val Array(size, _) = head.split(' ')
+          val newResult = unfoldPath(currentPath, result, (acc, p) => acc + (p -> (acc.getOrElse(p, 0) + size.toInt)))
+          sizeOfDir(tail, currentPath, newResult)
+        else
+          cdR.findFirstMatchIn(head) match
+            case Some(m) => sizeOfDir(tail, m.group(1) +: currentPath, result)
+            case None    => sizeOfDir(tail, currentPath, result) // matches "dir ..."
+      case Nil => result
+
+  def part1(lines: List[String]): Int =
+    sizeOfDir(lines, List.empty, Map.empty)
+      .filter(_._2 <= 100000)
+      .values
+      .sum
+
+  def part2(lines: List[String]): Int =
+    val dirs           = sizeOfDir(lines, List.empty, Map.empty)
+    val availableSpace = 70000000 - dirs(List("/"))
+    val toCleanup      = 30000000 - availableSpace
+    dirs.filter(_._2 >= toCleanup).minBy(_._2)._2
---
+++ src/test/scala/aoc/Day7Test.scala
@@ -0,0 +1,36 @@
+package aoc
+
+import zio.test.{ZIOSpecDefault, assertTrue}
+
+object Day7Test extends ZIOSpecDefault {
+  val input = List(
+    "$ cd /",
+    "$ ls",
+    "dir a",
+    "14848514 b.txt",
+    "8504156 c.dat",
+    "dir d",
+    "$ cd a",
+    "$ ls",
+    "dir e",
+    "29116 f",
+    "2557 g",
+    "62596 h.lst",
+    "$ cd e",
+    "$ ls",
+    "584 i",
+    "$ cd ..",
+    "$ cd ..",
+    "$ cd d",
+    "$ ls",
+    "4060174 j",
+    "8033020 d.log",
+    "5626152 d.ext",
+    "7214296 k",
+  )
+
+  override def spec = suite("Day 7")(
+    test("Part 1")(assertTrue(Day7.part1(input) == 95437)),
+    test("Part 2")(assertTrue(Day7.part2(input) == 24933642))
+  )
+}
