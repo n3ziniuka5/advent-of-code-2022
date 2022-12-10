4c03d7dad8a204452a9d8a774f5018179eb23747
n3ziniuka5 <laurynas.tretjakovas@gmail.com>
2022-12-10 12:12:46 +0200

    Reuse cycle logic between parts

--- src/main/scala/aoc/Day10.scala
+++ src/main/scala/aoc/Day10.scala
@@ -11,45 +11,44 @@ object Day10:
     timed("Part 1", part1(lines))
     timed("Part 2", part2(lines))
 
+  @tailrec
+  def cycle[A](lines: List[String], startCycle: Int, x: Int, acc: A, f: (Range, Int, A) => A): A =
+    lines match
+      case head :: tail =>
+        val (newCycle, newX) =
+          if (head == "noop") (startCycle + 1, x)
+          else (startCycle + 2, x + head.drop(5).toInt)
+
+        val newAcc = f(startCycle until newCycle, x, acc)
+        cycle(tail, newCycle, newX, newAcc, f)
+
+      case Nil => acc
+
   def part1(lines: List[String]): Int =
-    @tailrec
-    def loop(lines: List[String], startCycle: Int, x: Int, signalStrengths: List[Int]): List[Int] =
-      lines match
-        case head :: tail =>
-          val (newCycle, newX) =
-            if (head == "noop") (startCycle + 1, x)
-            else
-              val add = head.drop(5).toInt
-              (startCycle + 2, x + add)
-
-          val addedSignalStrengths = (startCycle until newCycle).toList.filter(c => c % 40 == 20).map(c => c * x)
-          loop(tail, newCycle, newX, addedSignalStrengths ++ signalStrengths)
-
-        case Nil => signalStrengths
-
-    loop(lines, 1, 1, List.empty).sum
+    cycle(
+      lines,
+      1,
+      1,
+      List.empty[Int],
+      { (cycles, x, signalStrengths) =>
+        val addedSignalStrengths = cycles.toList.filter(c => c % 40 == 20).map(c => c * x)
+        addedSignalStrengths ++ signalStrengths
+      }
+    ).sum
 
   def part2(lines: List[String]): String =
-    @tailrec
-    def loop(lines: List[String], startCycle: Int, x: Int, crtLines: Vector[Vector[Char]]): Vector[Vector[Char]] =
-      lines match
-        case head :: tail =>
-          val (newCycle, newX) =
-            if (head == "noop") (startCycle + 1, x)
-            else
-              val add = head.drop(5).toInt
-              (startCycle + 2, x + add)
-
-          val newCrt = (startCycle until newCycle).foldLeft(crtLines) { (acc, c) =>
-            val row    = (c - 1) / 40
-            val col    = (c - 1) % 40
-            val toDraw = if (((x - 1) to (x + 1)).contains(col)) '#' else ' '
-            acc.updated(row, acc(row).updated(col, toDraw))
-          }
-
-          loop(tail, newCycle, newX, newCrt)
-
-        case Nil => crtLines
-
-    val crtLines = loop(lines, 1, 1, Vector.fill(6)(Vector.fill(40)('!')))
+    val crtLines = cycle(
+      lines,
+      1,
+      1,
+      Vector.fill(6)(Vector.fill(40)('!')),
+      { (cycles, x, crtLines) =>
+        cycles.foldLeft(crtLines) { (crtLines, c) =>
+          val row    = (c - 1) / 40
+          val col    = (c - 1) % 40
+          val toDraw = if (((x - 1) to (x + 1)).contains(col)) '#' else ' '
+          crtLines.updated(row, crtLines(row).updated(col, toDraw))
+        }
+      }
+    )
     "\n" + crtLines.map(_.mkString).mkString("\n")
