dac2dcf29ff335cacf7d033baaa491151d136641
n3ziniuka5 <laurynas.tretjakovas@gmail.com>
2022-12-11 20:50:34 +0200

    implement lcm and refactor

--- src/main/scala/aoc/Day11.scala
+++ src/main/scala/aoc/Day11.scala
@@ -11,9 +11,10 @@ object Day11:
     timed("Part 1", part1(lines))
     timed("Part 2", part2(lines))
 
-  case class Monkey(items: Vector[Long], operation: Long => Long, test: Long => Boolean, ifTrue: Int, ifFalse: Int)
+  case class Monkey(items: Vector[Long], operation: Long => Long, test: Long => Boolean, ifTrue: Int, ifFalse: Int):
+    def withItem(item: Long): Monkey = copy(items = items :+ item)
 
-  def parse(lines: List[String]): Vector[Monkey] = {
+  def parseMonkeys(lines: List[String]): Vector[Monkey] =
     lines
       .sliding(6, 7)
       .map { monkeyInput =>
@@ -22,12 +23,11 @@ object Day11:
         val operation: Long => Long =
           if (operationString == "old * old") n => n * n
           else if (operationString.startsWith("old *"))
-            val mult = operationString.drop("old * ".length).toLong
-            n => n * mult
-          else if (operationString.startsWith("old +"))
+            val multiplier = operationString.drop("old * ".length).toLong
+            worry => worry * multiplier
+          else
             val add = operationString.drop("old + ".length).toLong
-            n => n + add
-          else n => n
+            worry => worry + add
 
         val divisibleBy = monkeyInput(3).drop("  Test: divisible by ".length).toInt
         val ifTrue      = monkeyInput(4).drop("    If true: throw to monkey ".length).toInt
@@ -36,33 +36,48 @@ object Day11:
         Monkey(items, operation, n => n % divisibleBy == 0, ifTrue, ifFalse)
       }
       .toVector
-  }
+
+  def lcm(n1: Int, n2: Int): Int =
+    (math.max(n1, n2) to Int.MaxValue).find { i =>
+      i % n1 == 0 && i % n2 == 0
+    }.get
+
+  def findLcm(lines: List[String]): Int =
+    lines
+      .sliding(6, 7)
+      .map { monkeyInput =>
+        monkeyInput(3).drop("  Test: divisible by ".length).toInt
+      }
+      .reduce(lcm)
 
   @tailrec
-  def play(rounds: Int, divideBy: Int, monkeys: Vector[Monkey], timesInspected: Map[Int, Long]): Map[Int, Long] = {
+  def play(
+    rounds: Int,
+    reliefDivision: Int,
+    divisorLcm: Int,
+    monkeys: Vector[Monkey],
+    timesInspected: Map[Int, Long]
+  ): Map[Int, Long] =
     if (rounds == 0) timesInspected
     else
-      val (newMonkeys, newInspected) = monkeys.indices.foldLeft((monkeys, timesInspected)) {
-        case ((monkeys, inspected), i) =>
-          val newMap = inspected + (i -> (inspected.getOrElse(i, 0L) + monkeys(i).items.size))
-          val newMonkeys = monkeys(i).items.foldLeft(monkeys) { (accm, item) =>
-            val newWorry = (monkeys(i).operation(item) / divideBy) % 9699690
+      val (monkeysAfterRound, newInspected) = monkeys.indices.foldLeft((monkeys, timesInspected)) {
+        case ((monkeysBeforeTurn, inspected), i) =>
+          val monkeysAfterTurn = monkeysBeforeTurn(i).items.foldLeft(monkeysBeforeTurn) { (monkeys, worryLevel) =>
+            val newWorry = (monkeys(i).operation(worryLevel) / reliefDivision) % divisorLcm
             val passTo   = if (monkeys(i).test(newWorry)) monkeys(i).ifTrue else monkeys(i).ifFalse
-            accm
-              .updated(passTo, accm(passTo).copy(items = accm(passTo).items :+ newWorry))
-              .updated(i, accm(i).copy(items = Vector.empty))
+            monkeys
+              .updated(passTo, monkeys(passTo).withItem(newWorry))
+              .updated(i, monkeys(i).copy(items = Vector.empty))
           }
-          (newMonkeys, newMap)
+          val newMap = inspected + (i -> (inspected.getOrElse(i, 0L) + monkeysBeforeTurn(i).items.size))
+          (monkeysAfterTurn, newMap)
       }
-      play(rounds - 1, divideBy, newMonkeys, newInspected)
-  }
+      play(rounds - 1, reliefDivision, divisorLcm, monkeysAfterRound, newInspected)
 
   def part1(lines: List[String]): Long =
-    val timesInspected = play(20, 3, parse(lines), Map.empty)
-    println(timesInspected)
+    val timesInspected = play(20, 3, findLcm(lines), parseMonkeys(lines), Map.empty)
     timesInspected.toList.map(_._2).sorted.reverse.take(2).product
 
   def part2(lines: List[String]): Long =
-    val timesInspected = play(10000, 1, parse(lines), Map.empty)
-    println(timesInspected)
+    val timesInspected = play(10000, 1, findLcm(lines), parseMonkeys(lines), Map.empty)
     timesInspected.toList.map(_._2).sorted.reverse.take(2).product
