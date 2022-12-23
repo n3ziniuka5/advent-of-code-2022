d95bc559e48f2966d5e450adafa852afd7f95c70
n3ziniuka5 <laurynas.tretjakovas@gmail.com>
2022-12-23 12:37:19 +0200

    Day 23, part 2

--- src/main/scala/aoc/Day23.scala
+++ src/main/scala/aoc/Day23.scala
@@ -15,10 +15,10 @@ object Day23:
   case class DirectionDelta(x: Int, y: Int)
   case class DirectionProposal(lookAt: List[DirectionDelta], moveInto: DirectionDelta)
   case class Point(x: Int, y: Int):
-    def neighbors: Set[Point] =
+    def neighbors: IndexedSeq[Point] =
       for {
-        dX <- Set(-1, 0, 1)
-        dY <- Set(-1, 0, 1) if dX != 0 || dY != 0
+        dX <- -1 to 1
+        dY <- -1 to 1 if dX != 0 || dY != 0
       } yield Point(x + dX, y + dY)
 
     def applyDelta(delta: DirectionDelta): Point = Point(x + delta.x, y + delta.y)
@@ -45,22 +45,14 @@ object Day23:
       )
     ) #::: proposedDirectionStream
 
-  def drawElves(elves: Set[Point]): String =
-    val minX = elves.minBy(_.x).x
-    val maxX = elves.maxBy(_.x).x
-    val minY = elves.minBy(_.y).y
-    val maxY = elves.maxBy(_.y).y
-
-    println(s"minX - $minX, maxX - $maxX, minY - $minY, maxY - $maxY")
-
-    (for {
-      y <- maxY to minY by -1
-      x <- minX to maxX
-    } yield if (elves.contains(Point(x, y))) '#' else '.').grouped(maxX - minX + 1).map(_.mkString).mkString("\n")
-
   @tailrec
-  def move(maxRounds: Int, elves: Set[Point], proposals: LazyList[DirectionProposal]): Set[Point] =
-    if (maxRounds == 0) elves
+  def move(
+    maxRounds: Int,
+    roundsPlayed: Int,
+    elves: Set[Point],
+    proposals: LazyList[DirectionProposal]
+  ): (Set[Point], Int) =
+    if (maxRounds == 0) (elves, roundsPlayed)
     else
       val shouldMoveTo = elves.map { location =>
         if (location.neighbors.forall(!elves.contains(_))) {
@@ -85,27 +77,20 @@ object Day23:
         else moveInto
       }
 
-      move(maxRounds - 1, newElves, proposals.tail)
+      if (newElves == elves)
+        (elves, roundsPlayed + 1)
+      else
+        move(maxRounds - 1, roundsPlayed + 1, newElves, proposals.tail)
 
   def part1(lines: List[String]): Int =
-
-    val initialElves = parse(lines)
-    val finalElves   = move(10, initialElves, proposedDirectionStream)
+    val (finalElves, _) = move(10, 0, parse(lines), proposedDirectionStream)
 
     val minX = finalElves.minBy(_.x).x
     val maxX = finalElves.maxBy(_.x).x
     val minY = finalElves.minBy(_.y).y
     val maxY = finalElves.maxBy(_.y).y
 
-    println
-    // println(drawElves(initialElves))
-    println
-    // println(drawElves(finalElves))
-    println
-
-    // println(s"minX - $minX, maxX - $maxX, minY - $minY, maxY - $maxY")
-
     (maxX - minX + 1) * (maxY - minY + 1) - finalElves.size
 
   def part2(lines: List[String]): Int =
-    0
+    move(Int.MaxValue, 0, parse(lines), proposedDirectionStream)._2
