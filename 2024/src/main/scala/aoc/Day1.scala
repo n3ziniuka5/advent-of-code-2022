cda427ba334584022dbfc8e75841cd0236fcefef
n3ziniuka5 <laurynas.tretjakovas@gmail.com>
2024-12-01 08:55:17 +0200

    AoC is back! Day1

--- 2024/src/main/scala/aoc/Day1.scala
+++ 2024/src/main/scala/aoc/Day1.scala
@@ -12,8 +12,14 @@ object Day1:
         timed("Part 1", part1(lines))
         timed("Part 2", part2(lines))
 
-    def part1(lines: List[String]): Int =
-        0
+    def part1(lines: List[String]): Long =
+        val firstList  = lines.map(line => line.split("\\s+")(0).toLong).sorted
+        val secondList = lines.map(line => line.split("\\s+")(1).toLong).sorted
+        firstList.zip(secondList).map((a, b) => math.abs(a - b)).sum
 
-    def part2(lines: List[String]): Int =
-        0
+    def part2(lines: List[String]): Long =
+        val firstList  = lines.map(line => line.split("\\s+")(0).toLong).sorted
+        val secondList = lines.map(line => line.split("\\s+")(1).toLong).sorted
+
+        val rightListCount = secondList.groupBy(identity).mapValues(_.length)
+        firstList.map(a => rightListCount.getOrElse(a, 0) * a).sum
--- 2024/src/test/scala/aoc/Day1Test.scala
+++ 2024/src/test/scala/aoc/Day1Test.scala
@@ -5,23 +5,15 @@ import zio.test.assertTrue
 
 object Day1Test extends ZIOSpecDefault:
     val input = List(
-      "1abc2",
-      "pqr3stu8vwx",
-      "a1b2c3d4e5f",
-      "treb7uchet",
-    )
-
-    val part2Input = List(
-      "two1nine",
-      "eightwothree",
-      "abcone2threexyz",
-      "xtwone3four",
-      "4nineeightseven2",
-      "zoneight234",
-      "7pqrstsixteen"
+      "3   4",
+      "4   3",
+      "2   5",
+      "1   3",
+      "3   9",
+      "3   3"
     )
 
     override def spec = suite("Day 1")(
-      test("Part 1")(assertTrue(Day1.part1(input) == 142)),
-      test("Part 2")(assertTrue(Day1.part2(part2Input) == 281))
+      test("Part 1")(assertTrue(Day1.part1(input) == 11)),
+      test("Part 2")(assertTrue(Day1.part2(input) == 31))
     )
